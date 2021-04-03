98edc804fd40744df25d31aa95ab90fe0b5f13ad
Brxxxl <lbruxxxel@gmail.com>
2021-04-03 19:19:55 -0300

    ...

--- Jogo.c
+++ Jogo.c
@@ -65,7 +65,7 @@ int main () {
 			}
 		}
 
-		{//	Criação dos coisos
+		{//	Criação dos componentes
 		
 			display = al_create_display(width, height);
 
@@ -75,21 +75,34 @@ int main () {
 
 				al_register_event_source(event_queue, al_get_display_event_source(display));
 				al_register_event_source(event_queue, al_get_keyboard_event_source());
+				al_register_event_source(event_queue, al_get_mouse_event_source());
 			}
-		}
+
+			{ // Tocar a música
+				mixer = al_create_mixer(44100, ALLEGRO_AUDIO_DEPTH_FLOAT32, ALLEGRO_CHANNEL_CONF_2);
+				voice = al_create_voice(44100, ALLEGRO_AUDIO_DEPTH_INT16, ALLEGRO_CHANNEL_CONF_2);
+				soundtrack = al_load_audio_stream("./Assets/Audio/Soundtrack.ogg",4,44800);
+
+					if (!soundtrack) {				//Debug
+						printf ("audio fail\n\n");
+					}
+
+				//al_set_audio_stream_playmode(soundtrack, ALLEGRO_PLAYMODE_LOOP);
+				//al_attach_audio_stream_to_mixer(soundtrack, mixer);
+				al_attach_mixer_to_voice(mixer, voice);
+			}
+
+			}
 
 		{//	Funções DEBUG
 			#ifdef DEBUG
 				if (al_is_keyboard_installed){
-					printf ("keyboard is installed\n");
+					//printf ("keyboard is installed\n");
 				}
 			#endif
 		}
 
-	#ifdef DEBUG
-		sleep (3);
-	#endif
-
+	sleep (3);
 	terminate(display);
 	return 0;
 }

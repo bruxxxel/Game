431349ce439ebbec5e6bf52b1f8d7483c599cecc
Brxxxl <lbruxxxel@gmail.com>
2021-04-03 17:16:38 -0300

    ...

--- Jogo.c
+++ Jogo.c
@@ -1,24 +1,6 @@
-//  Bibliotecas Padrão
-	#include <stdio.h>
-	#include <math.h>
-
-//  Bibliotecas Allegro
-	#include <allegro5/allegro.h>
-	#include <allegro5/allegro5.h>
-	//  Bibliotecas Gráficas
-		#include <allegro5/allegro_primitives.h>
-		#include <allegro5/allegro_image.h>
-	//	Bibliotecas de Áudio
-		#include <allegro5/allegro_audio.h>
-		#include <allegro5/allegro_acodec.h>
-	//  Bibliotecas de Hardware
-		#include <allegro5/keyboard.h>
-		#include <allegro5/keycodes.h>
-		#include <allegro5/mouse.h>
-		#include <allegro5/mouse_cursor.h>
-
 //	Definições
 	#define DEBUG
+	#include "AllegroHeader.h"
 
 void terminate (ALLEGRO_DISPLAY *display) {
 
